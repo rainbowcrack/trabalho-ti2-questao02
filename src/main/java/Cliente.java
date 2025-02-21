a4520b40112137355f5be4d6545769588d96da9a
Izabel Chaves <145824500+rainbowcrack@users.noreply.github.com>
2025-02-21 00:32:41 -0300

    Create Cliente.java

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,60 @@
+public class Cliente {
+    // promove heranca e encapsulamento
+    private int id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    // construtores
+    public Cliente(int id, String nome, String email, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public Cliente(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    // getters e setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    // polimorfismo
+    @Override
+    public String toString() {
+        return "Cliente{id=" + id + ", nome='" + nome + "', email='" + email + "', telefone='" + telefone + "'}";
+    }
+}
